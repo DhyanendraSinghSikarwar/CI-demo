2af2e2404374f8f039b6d4b2b32b3dfab4a30702
DhyanendraSinghSikarwar <dhyanendra.manit@gmail.com>
2025-08-13 13:24:15 +0530

    add multiply function

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 
 result1 = add(5, 3)
 result2 = sub(5, 3)
+result3 = mul(5, 3)
 
-print(result1, result2)
+print(result1, result2, result3)
