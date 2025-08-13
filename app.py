01068eaf31220338c4509421a72d1fffb0332854
DhyanendraSinghSikarwar <dhyanendra.manit@gmail.com>
2025-08-13 13:44:59 +0530

    add testing code

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def sub(a, b):
 def mul(a, b):
     return a * b
 
+if __name__ == '__main__':
+    result1 = add(5, 3)
+    result2 = sub(5, 3)
+    result3 = mul(5, 3)
 
-result1 = add(5, 3)
-result2 = sub(5, 3)
-result3 = mul(5, 3)
-
-print(result1, result2, result3)
+    print(result1, result2, result3)
